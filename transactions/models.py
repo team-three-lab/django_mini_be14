1239d804c32f09861a2494523fe16b3bbbe02fa0
tjdgusf <tjdgusf@gmail.com>
2025-11-22 22:58:10 +0900

    ✨ feat:모델추가

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+from django.db import models 
 
-# Create your models here.
+class Accounts(models.Model):
+    TYPE = (
+        ("DEPOSIT", "입금"),
+        ("WITHDRAW" "출금"),
+    )
+
+    # account_id = models.IntegerField() #나중에 fk로 연결 할 계획
+    type = models.CharField(max_length=20, choices=TYPE)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    result_amount = models.DecimalField(max_digits=12, decimal_places=2)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"타입: {self.type}, {self.type}금액: {self.amount}"
+    
+    class Meta:
+        db_table = "accounts"
+        verbose_name = "계좌 조회"
+        verbose_name_plural = "조회 목록"
