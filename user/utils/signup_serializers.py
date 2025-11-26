30afff04c1bc3d15bd1514a6c4f59defd710456a
dddops <Eopseum0@gmail.com>
2025-11-26 19:00:09 +0900

    fix: 비번 수정, 수정된 비번 해시화, 비밀 번호 확인(2번) 구현

--- user/utils/signup_serializers.py
+++ user/utils/signup_serializers.py
@@ -6,12 +6,21 @@ from django.core.exceptions import ValidationError
 User = get_user_model()
 
 class SignUpSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+    password2 = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
-        fields = ['email', 'password', 'nickname', 'name', 'phone_number']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['email', 'password', 'password2', 'nickname', 'name', 'phone_number']
 
     def validate(self, data):
+        password = data.get('password')
+        password2 = data.pop('password2', None)  # password2는 검증 후 제거 db에 저장 안함
+
+        if password and password2 and password != password2: # 회원 정보 수정시 password2가 없는 상태기 때문에 이부분 패스
+            raise serializers.ValidationError({
+                "password2": ["두 비밀번호가 일치하지 않습니다."]
+            })
         user = User(**data)
         errors = {}
 
@@ -34,3 +43,13 @@ class SignUpSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        instance = super().update(instance, validated_data)
+
+        if password:# 회원 정보 수정시 비번 해시화
+            instance.set_password(password)
+            instance.save()
+
+        return instance
